4fd39cba2ec6c74ba5b2df8116ca2599c6d2d59e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-04 18:19:57 +0300

    Add alert function on not filled areas

--- JS/script.js
+++ JS/script.js
@@ -7,11 +7,16 @@ $(document).ready(function () {
     var q3 = $("input:radio[name=q3]:checked").val();
     var q4 = $("input:radio[name=q4]:checked").val();
     var q5 = $("input:radio[name=q5]:checked").val();
+    if (!q1 || !q2 || !q3 || !q4 || !q5) {
+      alert("please ensure you have all your questions filled!")
+    }
+    else {
     var result = parseInt(q1) + parseInt(q2) +
       parseInt(q3) + parseInt(q4) + parseInt(q5);
     $("#result").text("You Score: " + result + "%");
     $("#showScore").show();
     $("#showTest").hide();
+    }
   });
   $("#reset").click(function() {
     location.reload();
